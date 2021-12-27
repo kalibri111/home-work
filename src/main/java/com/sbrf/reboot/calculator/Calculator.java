e4dca1ee7ff66756b254c27b0d3ab580888f4dd8
ivanleskin <livan.skin@yandex.ru>
2021-12-27 22:20:16 +0300

    Calculator class added

---
+++ src/main/java/com/sbrf/reboot/calculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.sbrf.reboot.calculator;
+import com.sbrf.reboot.Main;
+
+import java.util.*;
+
+public class Calculator {
+    public int getAddition(int first, int second) {
+        return first + second;
+    }
+
+
+    public int getSubtraction(int first, int second) {
+        return first - second;
+    }
+
+    public int getMultiplication(int first, int second) {
+        return first * second;
+    }
+
+    public int getDivision(int first, int second) {
+        return first / second;
+    }
+
+    public double sin(double angle) {
+        return Math.sin(angle);
+    }
+
+    public double cos(double angle) {
+        return Math.cos(angle);
+    }
+
+    public double tan(double angle) {
+        return Math.tan(angle);
+    }
+}
