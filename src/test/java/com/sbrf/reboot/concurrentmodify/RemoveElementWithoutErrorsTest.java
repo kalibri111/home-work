174a0274a71ba3e363b2119a9310fe6cae39486f
ivanleskin <livan.skin@yandex.ru>
2022-02-07 14:31:26 +0300

    8th homework

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.ListIterator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -31,7 +33,55 @@ public class RemoveElementWithoutErrorsTest {
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
 
+        list.removeIf(val -> val == 1);
+
+        assertEquals(list.size(), 2);
+    }
+
+    @Test
+    public void SuccessRemoveElementWithStoreWithoutErrors(){
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+        
+        List<Integer> found = new ArrayList<>();
+
+        for (Integer item: list) {
+            if (item == 1) {
+                found.add(item);
+            }
+        }
+
+        list.removeAll(found);
+
+        assertEquals(list.size(), 2);
+    }
+
+    @Test
+    public void SuccessRemoveElementWithIteratorsWithoutErrors(){
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        ListIterator<Integer> iterator = list.listIterator();
+
+        while(iterator.hasNext()) {
+            if (iterator.next() == 1) {
+                iterator.remove();
+            }
+        }
+
+        assertEquals(list.size(), 2);
     }
 
 }
